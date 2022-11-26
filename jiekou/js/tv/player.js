d2a7c6c50cadf5629c5ab7970efda812c6f67add
wanxunvip <115612608+wanxunvip@users.noreply.github.com>
2022-11-26 08:10:49 +0800

    Add files via upload

---
+++ jiekou/js/tv/player.js
@@ -0,0 +1,20 @@
+jks = [
+    { title: "云解析（超清）", "url": "https://jx.yparse.com/index.php?url=" },
+    { title: "爱豆解析", "url": "https://jx.aidouer.net/?url=" },
+    { title: "夜幕解析", "url": "https://www.yemu.xyz/?url=" },
+    { title: "m3u8 (解析)", "url": "https://jsap.attakids.com/?url=" },
+    { title: "CKMOV解析", "url": "https://www.ckmov.vip/api.php?url=" },
+    { title: "8090", "url": "https://www.8090g.cn/jiexi/?url=" },
+    { title: "lele全网解析", "url": "https://lecurl.cn/?url=" },  
+    { title: "H8", "url": "https://www.h8jx.com/jiexi.php?url=" },
+	
+    { title: "盘古COM", "url": "https://www.pangujiexi.com/jiexi/?url=" },
+    { title: "思古", "url": "https://jsap.attakids.com/?url=" },
+    { title: "盘古CC", "url": "http://www.pangujiexi.cc/jiexi.php?url=" },
+    { title: "Ckplayer", "url": "https://www.ckplayer.vip/jiexi/?url=" },
+    { title: '虾米解析', 'url': 'https://jx.xmflv.com/?url=' },
+    { title: 'TV解析[腾讯 (芒果)]', url: 'https://jx.m3u8.tv/jiexi/?url=', },
+    { title: 'OK解析', url: 'https://okjx.cc/?url=', },
+    { title: '解析la', url: 'https://api.jiexi.la/?url=', },
+
+];
